c8210f3d5d396ecc627b91551e3e2722957dffc5
bang-dev <ecosystemprogramminglanguage@gmail.com>
2024-08-27 16:33:34 +0700

    Add Dockerfile and docker compose files

---
+++ src/main/java/com/dev/blog/BackendMyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.dev.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMyBlogApplication.class, args);
+	}
+
+}
